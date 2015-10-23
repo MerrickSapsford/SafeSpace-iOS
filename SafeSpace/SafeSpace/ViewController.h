82ffa98f9f5c49c876be62a83090495254f54048
Merrick Sapsford <dev@merricksapsford.co.uk>
2015-10-23 22:36:31 +0100

    Initial Commit

---
+++ SafeSpace/SafeSpace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeSpace
+//
+//  Created by Merrick Sapsford on 23/10/2015.
+//  Copyright © 2015 Team Moonflop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SafeSpace/SafeSpace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SafeSpace
+//
+//  Created by Merrick Sapsford on 23/10/2015.
+//  Copyright © 2015 Team Moonflop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
